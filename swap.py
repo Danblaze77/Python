7a387acbd68f6bec4279fbe53e327eb0db17aae0
Jesson <91170791+Danblaze77@users.noreply.github.com>
2022-12-24 15:37:34 +0530

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,9 @@
+#swapping
+a=int(input("Enter a number"))
+b=int(input("Enter another number"))
+c=0
+c=b
+b=a
+a=c
+print("Number 1 after exchanging "+str(a))
+print("Number 2 after exchanging "+str(b))
